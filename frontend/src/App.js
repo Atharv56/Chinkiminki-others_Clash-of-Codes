fe4949fc718f1a8095156bdbfaf4ad41b190e5c7
HiyaJain22 <105354219+HiyaJain22@users.noreply.github.com>
2023-03-04 12:25:26 +0530

    node correction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './components/Home/Home';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    <Router>
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-        </Routes>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
