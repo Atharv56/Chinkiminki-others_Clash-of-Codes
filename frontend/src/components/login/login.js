24d5942cbec24a94b28d2f85e8e387ab282923fe
HiyaJain22 <105354219+HiyaJain22@users.noreply.github.com>
2023-03-04 20:12:57 +0530

    3.1

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,91 +1,54 @@
-import React, { useRef , useState} from 'react'
-import {Card, Form, Button, Container} from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
-import{signInWithEmailAndPassword} from 'firebase/auth'
-import {auth} from '../firebase'
-import AnimatedPage from '../Animate/Animate'
-export default function Login() {
-
-    const navigate = useNavigate()
-    // const [values, setValues] = useState({
-    //     email: "",
-    //     pass: ""
-    // })
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const [errorMsg, setErrorMsg]= useState('')
-
-
-    const handleSubmission= () =>{
-
-        signInWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value).then(()=>{
-            navigate('/home')
-        }).catch((err)=> {
-            setErrorMsg(err.message)
-            console.log("Error - ", err.message)
-    })
-    }
-
-  return (
-    <>
-    <AnimatedPage>
-    <Container className="d-flex align-items-center justify-content-center" style={{minHeight: '100vh'}}>
-        <div className='w-100' style={{maxWidth: '400px'}}>
-        <Card>
-            <Card.Body>
-                <h2 className='text-center mb-4'>Login</h2>
-                <h6>{errorMsg}</h6>
-                <Form>
-                    <Form.Group id ='email'>
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type='email' ref = {emailRef} required />
-                    </Form.Group>
-                    <Form.Group id ='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control  type='password' ref = {passwordRef} required />
-                    </Form.Group>
-                    <Button type="button" onClick={handleSubmission} className='w-100 mt-2'>Log in
-                    </Button>
-    
-                </Form>
-            </Card.Body>
-        </Card>
-        <div className='w-100 text-center mt-2'>
-            Don't have an account? <Link to="/">Sign Up</Link> 
-        </div>
-        </div>
-        </Container>
-        </AnimatedPage>
-    </>
-  )
-}
-
-
-
-    // const {signup} = useAuth()
-    // const [error, setError] = useState("")
-    // const [loading, setLoading] = useState(false)
-
-    // async function handleSubmit(e){
-    //     e.preventDefault()
-
-    //     if(passwordRef.current.value !==
-    //         passwordConfirmRef.current.value){
-    //             return setError('Passwords do not match')
-    //         }
-    //         try{
-    //             setError('')
-    //             setLoading(true)
-    //            await signup(emailRef.current.value, passwordRef.current.value)
-    //         }
-    //         catch{
-    //             setError('Failed to create an account')
-    //         }
-    //         setLoading(false)
-
-    //     signup(emailRef.current.value, passwordRef.current.value)
-    // }
-
-
-
-    
+import {
+    Button,
+    Checkbox,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Link,
+    Stack,
+    Image,
+  } from '@chakra-ui/react';
+  
+  export default function SplitScreen() {
+    return (
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+          <Stack spacing={4} w={'full'} maxW={'md'}>
+            <Heading fontSize={'2xl'}>Sign in to your account</Heading>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Link color={'blue.500'}>Forgot password?</Link>
+                <Link color={'blue.500'} href="/">Sign up</Link>
+              </Stack>
+              <Button colorScheme={'blue'} variant={'solid'}>
+                Sign in
+              </Button>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex flex={1}>
+          <Image
+            alt={'Login Image'}
+            objectFit={'cover'}
+            src={
+              'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+            }
+          />
+        </Flex>
+      </Stack>
+    );
+  }
--- frontend/src/components/sign up/signup.js
+++ frontend/src/components/sign up/signup.js
@@ -1,109 +1,57 @@
-import React, { useRef, useState } from 'react'
-import {Card, Form, Button, Container} from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
-import{createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
-import {auth} from '../firebase'
-import AnimatedPage from '../Animate/Animate'
-
-export default function Signup() {
-    const navigate = useNavigate()
-    // const [values, setValues] = useState({
-    //     name: "",
-    //     email: "",
-    //     pass: ""
-    // })
-    const emailRef = useRef()
-    const passwordRef = useRef()
-    const nameRef = useRef()
-    const [errorMsg, setErrorMsg]= useState('')
-
-    const handleSubmission= () =>{
-        console.log(emailRef.current.value)
-
-        createUserWithEmailAndPassword(auth, emailRef.current.value, passwordRef.current.value).then(async(res)=>{
-            const user = res.user
-            await updateProfile(user, {
-                displayName: nameRef.current.value,
-            });
-            console.log(user)
-            navigate('/login')
-        }).catch((err)=> {
-            setErrorMsg(err.message)
-            console.log("Error - ", err.message)
-    
-    })
-    }
-    
-  return (
-    <>
-    <AnimatedPage>
-    <Container className="d-flex align-items-center justify-content-center" style={{minHeight: '100vh'}}>
-        <div className='w-100' style={{maxWidth: '400px'}}>
-        <Card>
-            <Card.Body>
-                <h2 className='text-center mb-4'>Sign Up</h2>
-                <h6>{errorMsg}</h6>
-                
-                <Form>
-                <Form.Group id ='text'>
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control type='text' ref = {nameRef} required />
-                    </Form.Group>
-                    <Form.Group id ='email'>
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type='email' ref = {emailRef} required />
-                    </Form.Group>
-                    <Form.Group id ='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type='password' ref = {passwordRef} required />
-                    </Form.Group>
-                    {/* <Form.Group id ='password-confirm'>
-                        <Form.Label>Password Conformation</Form.Label>
-                        <Form.Control type='password' ref = {passwordConfirmRef} required />
-                    </Form.Group> */}
-                    <Button type="button" className='w-100 mt-2' onClick={handleSubmission}>Sign Up
-                    </Button>
-    
-                </Form>
-            </Card.Body>
-        </Card>
-        <div className='w-100 text-center mt-2'>
-            Already have an account? <Link to="/login">Log In</Link> 
-        </div>
-        </div>
-        </Container>
-        </AnimatedPage>
-    </>
-  )
-}
-
-
-
-
-
-
-
-// const passwordConfirmRef = useRef()
-    // const {signup} = useAuth()
-    // const [error, setError] = useState("")
-    // const [loading, setLoading] = useState(false)
-
-    // async function handleSubmit(e){
-    //     e.preventDefault()
-
-    //     if(passwordRef.current.value !==
-    //         passwordConfirmRef.current.value){
-    //             return setError('Passwords do not match')
-    //         }
-    //         try{
-    //             setError('')
-    //             setLoading(true)
-    //            await signup(emailRef.current.value, passwordRef.current.value)
-    //         }
-    //         catch{
-    //             setError('Failed to create an account')
-    //         }
-    //         setLoading(false)
-
-    //     signup(emailRef.current.value, passwordRef.current.value)
-    // }
+import {
+    Button,
+    Checkbox,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Link,
+    Stack,
+    Image,
+  } from '@chakra-ui/react';
+  
+  export default function SplitScreen() {
+    return (
+      <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+        <Flex p={8} flex={1} align={'center'} justify={'center'}>
+          <Stack spacing={4} w={'full'} maxW={'md'}>
+            <Heading fontSize={'2xl'}>Create an account</Heading>
+            <FormControl id="email">
+              <FormLabel>Name</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={6}>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Link color={'blue.500'} href="\login">Already have an account? Log in</Link>
+              </Stack>
+              <Button colorScheme={'blue'} variant={'solid'}>
+                Sign Up
+              </Button>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex flex={1}>
+          <Image
+            alt={'Login Image'}
+            objectFit={'cover'}
+            src={
+              "https://unsplash.com/photos/guNIjIuUcgY?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            }
+          />
+        </Flex>
+      </Stack>
+    );
+  }
