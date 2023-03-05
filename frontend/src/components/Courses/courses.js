7dc4af8aa4f95a0f593a01b30e33ba1efa392fbc
bhuvighosh3 <108955148+bhuvighosh3@users.noreply.github.com>
2023-03-05 10:58:18 +0530

    hi

--- frontend/src/components/Courses/courses.js
+++ frontend/src/components/Courses/courses.js
@@ -2,17 +2,32 @@ import React from 'react'
 import NavScrollExample from '../navbar/Navbar';
 import { Card, CardHeader, CardBody, CardFooter,SimpleGrid,Heading, Text, Button } from '@chakra-ui/react'
 import Im from '../images/courses.png'
+import Im4 from '../images/courses2.png'
+import Im2 from '../images/courses3.png'
+import Im3 from '../images/courses3.png'
 import "./courses.css"
 
 export default function courses() {
+  var bg = {
+    backGroundImage: `url(${Im})`
+  }
+  var bg3 = {
+    backGroundImage: `url(${Im3})`
+  }
+  var bg2 = {
+    backGroundImage: `url(${Im2})`
+  }
+  var bg3 = {
+    backGroundImage: `url(${Im4})`
+  }
   return (
     <>
         <NavScrollExample />
         <SimpleGrid spacing={7} templateColumns='repeat(auto-fill, minmax(200px, 1fr))'>
-  <section style={{display:'flex',width:1150,height:250,padding:15,marginLeft:80,backGroundImage:Im,marginTop:90}}>
-  <Card>
+  <section style={{display:'flex',width:1150,height:250,padding:15,marginLeft:80,marginTop:90}}>
+  <Card id="c4">
     <CardHeader>
-      <Heading size='md'>Adventure Filmmaking</Heading>
+      <Heading size='md'></Heading>
     </CardHeader>
     <CardBody>
       <Text>View a summary of all your customers over the last month.</Text>
@@ -21,9 +36,9 @@ export default function courses() {
       <Button>View here</Button>
     </CardFooter>
   </Card>
-  <Card>
+  <Card id="c6">
     <CardHeader>
-      <Heading size='md'>Mapping Rajasthan</Heading>
+      <Heading size='md'></Heading>
     </CardHeader>
     <CardBody>
       <Text>View a summary of all your customers over the last month.</Text>
@@ -32,9 +47,9 @@ export default function courses() {
       <Button>View here</Button>
     </CardFooter>
   </Card>
-  <Card>
+  <Card id="c7">
     <CardHeader>
-      <Heading size='md'>Shikara Experience</Heading>
+      <Heading size='md'></Heading>
     </CardHeader>
     <CardBody>
       <Text>View a summary of all your customers over the last month.</Text>
@@ -43,9 +58,9 @@ export default function courses() {
       <Button>View here</Button>
     </CardFooter>
   </Card>
-  <Card>
+  <Card id="c8">
     <CardHeader>
-      <Heading size='md'>Mindful Journaling</Heading>
+      <Heading size='md'></Heading>
     </CardHeader>
     <CardBody>
       <Text>View a summary of all your customers over the last month.</Text>
@@ -57,9 +72,9 @@ export default function courses() {
   </section>
 </SimpleGrid>
 <section style={{display:'flex',width:875,height:250,padding:15,marginLeft:190}}>
-<Card>
+<Card id="c3">
     <CardHeader>
-      <Heading size='md'>East Explained</Heading>
+      <Heading size='md'></Heading>
     </CardHeader>
     <CardBody>
       <Text>View a summary of all your customers over the last month.</Text>
@@ -68,9 +83,9 @@ export default function courses() {
       <Button>View here</Button>
     </CardFooter>
   </Card>
-  <Card>
+  <Card id="c2">
     <CardHeader>
-      <Heading size='md'>Language Learning Guide</Heading>
+      <Heading size='md'></Heading>
     </CardHeader>
     <CardBody>
       <Text>View a summary of all your customers over the last month.</Text>
@@ -79,9 +94,9 @@ export default function courses() {
       <Button>View here</Button>
     </CardFooter>
   </Card>
-  <Card>
+  <Card id="c1">
     <CardHeader>
-      <Heading size='md'>How to Vlog?</Heading>
+      <Heading size='md'></Heading>
     </CardHeader>
     <CardBody>
       <Text>View a summary of all your customers over the last month.</Text>
